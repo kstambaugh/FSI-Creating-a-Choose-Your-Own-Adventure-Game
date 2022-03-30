438593dc61febedf7f6aa1a45639f95c78d3198b
kstambaugh <refinancethekraken@gmail.com>
2022-03-29 21:39:13 -0400

    first changes

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 //Your Code here
+let direction = window.prompt('Do you head left or right?')
+
+if(direction === "left"){
+    console.log("you go left")
+}
+if(direction === "right"){
+    console.log("you you right")
+}
